eb83d6de3fb86909cd8f598e47cada0c7674c5e9
chelseXie <xiehao35755@163.com>
2018-06-21 23:18:35 +0800

    2318

--- src/main/java/com/xiehao/aspect/proxy/impl/DefaultInvocationHandler.java
+++ src/main/java/com/xiehao/aspect/proxy/impl/DefaultInvocationHandler.java
@@ -22,19 +22,26 @@ public class DefaultInvocationHandler implements InvocationHandler {
     private Map<String,String> wovenMethods;
     //目标对象符合织入条件的方法
     private List<String> targetMethods;
+    //是否满足织入条件
+    private boolean status = false;
 
     public Object getInstance(Object target,Object wovenObject,Map wovenMethods,List<String> targetMethods){
         this.target = target;
         this.wovenObject = wovenObject;
         this.wovenMethods = wovenMethods;
         this.targetMethods = targetMethods;
+        if(target!=null&&wovenObject!=null&&wovenMethods!=null
+                &&wovenMethods.size()>0&&targetMethods!=null&&targetMethods.size()>0){
+            status = true;
+        }
         Class clazz = target.getClass();
         Object obj = Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(), this);
         return obj;
     }
+    @Override
     public Object  invoke(Object proxy, Method method, Object[] args) throws Throwable {
         //before执行
-        if(targetMethods.contains(method.getName())){
+        if(status&&targetMethods.contains(method.getName())){
             if(wovenMethods.containsKey(BEFORE)){
                 methodHandler(wovenMethods.get(BEFORE));
             }
@@ -42,7 +49,7 @@ public class DefaultInvocationHandler implements InvocationHandler {
         // 执行目标对象的方法
         Object result = method.invoke(target, args);
         //After执行
-        if(targetMethods.contains(method.getName())){
+        if(status&&targetMethods.contains(method.getName())){
             if(wovenMethods.containsKey(AFTER)){
                 methodHandler(wovenMethods.get(AFTER));
             }
--- src/main/java/com/xiehao/aspect/proxy/impl/JavaDynamicProxyFactory.java
+++ src/main/java/com/xiehao/aspect/proxy/impl/JavaDynamicProxyFactory.java
@@ -1,67 +1,1 @@
-package com.xiehao.aspect.proxy.impl;
-
-import com.xiehao.aspect.annotation.Aspect;
-import com.xiehao.aspect.annotation.AspectDes;
-import com.xiehao.aspect.bean.BeanScanner;
-import com.xiehao.aspect.bean.impl.BeanScannerImpl;
-import com.xiehao.aspect.proxy.ProxyFactory;
-import com.xiehao.aspect.proxy.ProxyUtils;
-
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Proxy;
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * java动态代理工厂
- * Created by next on 2018/6/20.
- */
-public class JavaDynamicProxyFactory implements ProxyFactory{
-    private DefaultInvocationHandler invocationHandler = null;
-    //扫描类
-    private BeanScanner scanner = null ;
-    //解析切面规则类
-
-    String packageBase = "" ;
-
-    private List<AspectDes> aspectDeslist = null;
-
-
-
-    public void  init(){
-        //初始化代理类
-        if(invocationHandler == null){
-
-           invocationHandler = new DefaultInvocationHandler();
-        }
-        //扫描器
-        if(scanner ==null){
-            scanner = new BeanScannerImpl();
-        }
-        List<Class> wovenClasss = scanner.getMatherClassList(packageBase, (x) -> {
-                    return ((Class) x).getAnnotation(Aspect.class) != null;
-                }
-        );
-        if(aspectDeslist ==null){
-            aspectDeslist = new ArrayList();
-        }
-        if(wovenClasss !=null && wovenClasss.size()>0){
-            for(Class wovenClass : wovenClasss){
-                aspectDeslist.add(ProxyUtils.convert(wovenClass));
-            }
-        }
-    }
-
-
-    public <T> T getProxyBean(T oject) {
-
-        invocationHandler.getInstance(oject,null,null,null);
-        Class<?> stuProxyClass = Proxy.getProxyClass(oject.getClass().getClassLoader(), new Class<?>[]{oject.getClass()});
-
-        return null;
-    }
-    private void initAspect(){
-
-    }
-
-}
+package com.xiehao.aspect.proxy.impl;import com.xiehao.aspect.annotation.Aspect;import com.xiehao.aspect.annotation.AspectDes;import com.xiehao.aspect.bean.BeanScanner;import com.xiehao.aspect.bean.LifeCycleStatus;import com.xiehao.aspect.bean.impl.BeanScannerImpl;import com.xiehao.aspect.proxy.ProxyFactory;import com.xiehao.aspect.proxy.ProxyUtils;import java.lang.reflect.InvocationHandler;import java.lang.reflect.Method;import java.lang.reflect.Proxy;import java.util.ArrayList;import java.util.List;import java.util.Objects;/** * java动态代理工厂 * Created by next on 2018/6/20. */public class JavaDynamicProxyFactory implements ProxyFactory{    private LifeCycleStatus status;    private DefaultInvocationHandler invocationHandler = null;    //扫描类    private BeanScanner scanner = null ;    //解析切面规则类    String packageBase = "" ;    private List<AspectDes> aspectDeslist = null;    public void  init(){        //初始化代理类        if(invocationHandler == null){           invocationHandler = new DefaultInvocationHandler();        }        //扫描器        if(scanner ==null){            scanner = new BeanScannerImpl();        }        List<Class> wovenClasss = scanner.getMatherClassList(packageBase, (x) -> {                    return ((Class) x).getAnnotation(Aspect.class) != null;                }        );        if(aspectDeslist ==null){            aspectDeslist = new ArrayList();        }        if(wovenClasss !=null && wovenClasss.size()>0){            for(Class wovenClass : wovenClasss){                aspectDeslist.add(ProxyUtils.convert(wovenClass));            }        }        this.status =LifeCycleStatus.INITED;    }    public <T> T getProxyBean(Class<T> clazz) {        if(LifeCycleStatus.INITED != this.status){            init();        }        Object object = invocationHandler.getInstance(oject,null,null,null);        return null;    }    /**     * 匹配是不是这个     */    private AspectDes matherAspect(Class c){        if(aspectDeslist !=null && aspectDeslist.size()>0){            for(AspectDes aspectDes :aspectDeslist){                aspectDes.getMatchingExpression()            }        }        return null;    }    private boolean mather(AspectDes aspectDes,Class clazz ){        if(aspectDes.getMatchingExpression()!=null && aspectDes.getMatchingExpression().length()>0){            String[] expres = aspectDes.getMatchingExpression().split(".");            String typeName = clazz.getTypeName();            String[] typeNames = typeName.split(".");            if(expres.length-1==typeNames.length){                for(int index = 0; index<expres.length-1;index++){                    if(expres[index].equals("*")){                       continue;                    }                    if(!expres[index].equalsIgnoreCase(typeNames[index])){                        return  false;                    }                }                if(expres[expres.length-1].endsWith("*")){                    return true;                }                Method[] methods = clazz.getMethods();                if(methods!=null && methods.length>0){                    for(Method method:methods){                        if(expres[expres.length-1].endsWith("*")){                            if(method.getName().startsWith(expres[expres.length-1].replace("*",""))){                                return true;                            }                        }                    }                }            }        }        return false;    }}
